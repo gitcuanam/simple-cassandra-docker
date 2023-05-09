31726fb4b07668c2b7a2a472ac0b3dac19a691e7
gitcuanam <nam.tranphuong.evotek@gmail.com>
2023-05-09 23:50:28 +0700

    add multithread to connect db

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import logging
+import threading
+import time
+from db import connect
+
+if __name__ == "__main__":
+    print("hi")
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=connect, args=(), daemon=True)
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    x.join()
+    logging.info("Main    : all done")
